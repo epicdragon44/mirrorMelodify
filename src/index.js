cf00a7fb9561399e8057702553f4f1a57f9a3b11
Haoli <haoliyin2002@gmail.com>
2020-04-11 14:43:45 -0500

    Make App.js and styles sheet

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './Components/App';
+import './styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
