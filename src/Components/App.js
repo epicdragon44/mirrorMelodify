2789fc54ed3edf6a02f75c3af5d6465500e87553
Haoli <haoliyin2002@gmail.com>
2020-04-11 15:19:30 -0500

    CSS Works

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import "./styles.css";
+
 
 function App(){
-    return ;
+    return <div>
+        <h1 className="heading">Hi! Welcome to Melodify.</h1>
+        <p> Melodify uses scientifically proven techniques to help you relax.</p>
+        <p>Customize your "sound bath," and when you're ready, hit Start. Then, close your eyes and relax for 45 minutes.</p>
+    </div>;
 }
+
+export default App;
