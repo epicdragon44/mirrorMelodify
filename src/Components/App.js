24b2dd26ac4c45e9b577d3fa6f7a17fd4cda1051
Haoli <haoliyin2002@gmail.com>
2020-04-11 17:27:57 -0500

    Add Navbar and button, still need to put in links for other pages

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./styles.css";
 
-
 function App(){
-    return <div>
-        <h1 className="heading">Hi! Welcome to Melodify.</h1>
-        <p> Melodify uses scientifically proven techniques to help you relax.</p>
-        <p>Customize your "sound bath," and when you're ready, hit Start. Then, close your eyes and relax for 45 minutes.</p>
+    function handleClick () {
+        console.log("Get Started");
+    }
+    return <div className="div">
+        <nav>
+            <ul>
+                <li className="left"><img src="https://live.staticflickr.com/65535/49761891423_a0ddd4d48e_b.jpg" alt="Melodify Logo" className="logo-img"/></li>
+                <li><a>Contact Us</a></li>
+                <li><a>About Us</a></li>
+                <li><a>Research</a></li>
+                <li><a>Music Generator</a></li>
+                <li className="active"><a href="#">Home</a></li>
+
+            </ul>
+        </nav>
+        <h1 className="header">Hi! Welcome to Melodify.</h1>
+        <p className="paragraph">Melodify uses scientifically proven techniques to help you relax.</p>
+        <p className="paragraph">Customize your "sound bath," and when you're ready, hit Start.</p>
+        <p className="paragraph">Then, close your eyes and relax for 45 minutes.</p>
+        <button onClick={handleClick} className="button">Get Started</button>
     </div>;
 }
 
