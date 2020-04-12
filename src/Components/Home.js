67dadf1fa72336f5b9bd40fdfd7f8582caddceda
Haoli <haoliyin2002@gmail.com>
2020-04-11 20:13:25 -0500

    Change CSS

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./styles.css";
-import { NavLink } from "react-router-dom";
 function Home() {
 
     return <div className="div">
-        <h1 className="header">Hi! Welcome to Melodify.</h1>
+        <h1 className="heading">Hi! Welcome to Melodify</h1>
         <p className="paragraph">Melodify uses scientifically proven techniques to help you relax.</p>
         <p className="paragraph">Customize your "sound bath," and when you're ready, hit Start.</p>
         <p className="paragraph">Then, close your eyes and relax for 45 minutes.</p>
+        <p className="paragraph">Click the Music Generator button to start your relaxing experience</p>
     </div>;
 }
 export default Home;
