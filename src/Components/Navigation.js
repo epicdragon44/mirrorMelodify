48ff214de9ab27bb3bb484406164d0d41ae8c11c
Haoli <haoliyin2002@gmail.com>
2020-04-12 00:57:29 -0500

    Fix Nav Logo

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./styles.css";
+
 function Navigation () {
     return (
         <nav>
             <ul>
-                <li className="left"><img src="https://live.staticflickr.com/65535/49761891423_a0ddd4d48e_b.jpg"
-                                          alt="Melodify Logo" className="logo-img"/></li>
+                <li className="left"><h1 className="leftheader">Melödify</h1></li>
                 <li><NavLink to="/AboutUs">About Us</NavLink></li>
                 <li><NavLink to="/Research">Research</NavLink></li>
                 <li><NavLink to="/MusicGenerator">Music Generator</NavLink></li>
