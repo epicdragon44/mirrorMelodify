20733aff8425d62044a8215d42f888c7b1d9a8af
Haoli <haoliyin2002@gmail.com>
2020-04-12 01:23:41 -0500

    Small Adjustments to Home Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,8 +6,7 @@ function Home() {
         <h1 className="header">Melodify</h1>
         <h2 className="header2">Science. Technology. Music.</h2>
         <p className="paragraph">Melodify uses scientifically proven techniques to help you relax.</p>
-        <p className="paragraph">Customize your "sound bath," and when you're ready, hit Start.</p>
-        <p className="paragraph">Then, close your eyes and relax for 45 minutes.</p>
+        <p className="paragraph">Customize your "sound bath," and when you're ready, hit start, close your eyes and relax for 45 minutes.</p>
         <p className="paragraph">Click the Music Generator button to start your relaxing experience</p>
     </div>;
 }
